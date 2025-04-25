8fde6a62f226bca66b12c44b0480467a1f6b528b
Rallende <rene.allende15@gmail.com>
2025-04-25 04:44:46 -0400

    .

--- core/views.py
+++ core/views.py
@@ -5,6 +5,6 @@ from rest_framework.response import Response
 class TestView(APIView):
     
     def get(self, request, *args, **kwargs):
-        return Response('Hola')
+        return Response('Hola Mundo')
         
     
